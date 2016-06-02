4379a87c7c5694fb7e81c36278ef01760f307355
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-06-02 20:59:06 +0900

    #2 Edit Programming

--- app/src/main/java/kr/hs/emirim/sinyh104/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.emirim.sinyh104.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,9 +21,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         editUrl=(EditText)findViewById(R.id.edit_url);
         butMove=(Button)findViewById(R.id.but_move);
-        butMove=(Button)findViewById(R.id.but_prev);
-        web=(WebView)findViewById(R.id.web);
+        butPrev=(Button)findViewById(R.id.but_prev);
+
         butMove.setOnClickListener(this);
+        butPrev.setOnClickListener(this);
+        web = (WebView) findViewById(R.id.web);
+        web.setWebViewClient(new WebViewClient());
+
+        WebSettings webSet=web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+
+        web.loadUrl("http://www.naver.com");
+
     }
 
 
@@ -32,6 +44,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-
+        String url=null;
+        switch(v.getId()) {
+            case R.id.but_move:
+                url=editUrl.getText().toString();
+                if(!url.contains("http"))
+                    url="http://"+url;
+                web.loadUrl(url);
+                break;
+            case R.id.but_prev:
+                web.goBack();
+                break;
+        }
     }
 }
